2993ed5849782b0b61e527ad62bcd071420d67d6
Smithy <121695494+ssmithy@users.noreply.github.com>
2023-02-18 22:21:08 +0000

    add package and webpack configured to produce simple html page with index.js

---
+++ PocketMoneyChores/PocketMoneyChores.Web/webpack.config.js
@@ -0,0 +1,18 @@
+﻿const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+
+    entry: {
+        main: path.resolve(__dirname, "./src/index.js"),
+    },
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "wwwroot")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack Output",
+        }),
+    ],
+};
